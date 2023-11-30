597863c22eacdbcd96088347d542deea16b4e20f
Nikhil-chhajer <nikhil.chhajer80@gmail.com>
2023-11-30 20:18:00 +0530

    Setupp of SMTP using nodemailer done

---
+++ src/services/email-service.js
@@ -0,0 +1,19 @@
+const sender=require('../config/emailConfig');
+const sendBasicEmail=async (mailfrom,mailTo,mailSubject,mailBody)=>{
+    try {
+        const response=await sender.sendMail({
+            from:mailfrom,
+            to:mailTo,
+            subject:mailSubject,
+            text:mailBody
+        });
+    } catch (error) {
+        console.log(error);
+        
+    }
+    //console.log(response);
+}
+
+module.exports={
+    sendBasicEmail
+} 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,26 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const {PORT}=require('../config/serverConfig');
-
+const{sendBasicEmail}=require('../services/email-service');
 const setupAndStartServer=()=>{
     const app=express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
         console.log("Server started at PORT",PORT);
+        sendBasicEmail(
+            'support@admin.com',
+            'nikhil.chhajer80@gmail.com',
+            'This is testing email',
+            'Hey,how are you,i hope you like the support'
+        );
 
-    })
+    });
+    // sendBasicEmail(
+    //     'support@admin.com',
+    //     'movie.mela80@gmail.com',
+    //     'This is testing email',
+    //     'Hey,how are you,i hope you like the support'
+    // );
 }
 setupAndStartServer();
